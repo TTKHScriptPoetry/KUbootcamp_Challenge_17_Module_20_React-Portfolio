e04dac8f988ec5219d230276370036ef110b561d
= <=>
2022-05-15 17:38:17 -0500

    Added form validation - required fields and email validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,18 +1,44 @@
 import React, { useState } from 'react'
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
   const { name, email, message } = formState; //the property names of formState -- destructure the formState object into its named properties
+  const [errorMessage, setErrorMessage] = useState(''); // initial state of the errorMessage is an empty string
 
   function handleChange(e) {
+    // -- Add validation in the handleChange function, allowing 
+    // validation of the form data before syncing the form data with the state, formState
+
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // assign the error message for the email input based on the isValid value
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    console.log('errorMessage', errorMessage);
+
     // -- We use the spread operator, ...formState, so we can retain the other key-value pairs in this object. 
     // without the spread operator, the formState object would be overwritten to only contain the name: value key pair.   
+    // setFormState({...formState, name: e.target.value }) // for name only
 
-    // -- The name property of target = name attribute of the form's input element
-    setFormState({...formState, [e.target.name]: e.target.value })
-    
-  }
-
+    // -- The name property of target e = name attribute of the form's input element
+    // Only allows the state to update with the user input if there is no error message
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  } // end of handleChange 
+  
   // -- Print test - not to be removed
   console.log(formState); // awesome
 
@@ -22,19 +48,24 @@ function ContactForm() {
       <form id="contact-form">
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" onChange={handleChange}  defaultValue={formState.name} />
+          <input type="text" name="name" onBlur={handleChange}  defaultValue={name} />
         </div>
 
         <div>
           <label htmlFor="email" >Email address:</label>
-          <input type="email" name="email" onChange={handleChange} defaultValue={formState.email} />
+          <input type="email" name="email" onBlur={handleChange} defaultValue={email} />
         </div>
 
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" onChange={handleChange} defaultValue={formState.message}/>
+          <textarea name="message" rows="5" onBlur={handleChange} defaultValue={message}/>
         </div>
-
+        {/* AND operator explained: If the first value resolves to true, the second expression is evaluated. */}
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
         <button type="submit">Submit</button>
 
       </form>
