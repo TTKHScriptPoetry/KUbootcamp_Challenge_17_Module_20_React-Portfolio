0fcc13e5c5d3897b67ea7a70817c65cd5f9baec3
MercuryKTgithub <KimDotnetPossible@att.net>
2022-06-14 03:05:20 -0500

    unwanted codes clean up

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -85,35 +85,7 @@ function ContactForm() {
       </div>
     </section>
     </div>
-
-
-//     <section>
-//       <h1>Contact me</h1>
-//       <form id="contact-form">
-//         <div>
-//           <label htmlFor="name">Name:</label>
-//           <input type="text" name="name" onBlur={handleChange}  defaultValue={name} />
-//         </div>
-// 
-//         <div>
-//           <label htmlFor="email" >Email address:</label>
-//           <input type="email" name="email" onBlur={handleChange} defaultValue={email} />
-//         </div>
-// 
-//         <div>
-//           <label htmlFor="message">Message:</label>
-//           <textarea name="message" rows="5" onBlur={handleChange} defaultValue={message}/>
-//         </div>
-//         
-//         {errorMessage && (
-//           <div>
-//             <p className="error-text">{errorMessage}</p>
-//           </div>
-//         )}
-//         <button type="submit">Submit</button>
-// 
-//       </form>
-//     </section>
+ 
   )
 }
 
